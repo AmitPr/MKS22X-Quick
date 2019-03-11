4de14271795af849a663d896df94e7cdfcf0dddc
AmitPr <17amitprasad@gmail.com>
2019-03-11 18:11:32 -0400

    Finished quickselect() and tester

--- Quick.java
+++ Quick.java
@@ -1,8 +1,41 @@
+import java.util.Arrays;
 import java.util.Random;
 
 public class Quick {
     public static void main(String[] args) {
-        System.out.println(partition(new int[] { 8, 6, 7, 5, 3, 0, 9 }, 0, 6));
+        int size = 1000;
+        int[] test = new int[size];
+        for (int i = 0; i < size; i++) {
+            test[i] = new Random().nextInt(1000000);
+        }
+        int[] sorted = Arrays.copyOf(test, size);
+        Arrays.sort(sorted);
+        for (int i = 0; i < size; i++) {
+            int x = new Random().nextInt(size);
+            if (quickselect(test, x) != sorted[x]) {
+                System.out.println("FAIL");
+            } else {
+                System.out.println("SUCCESS");
+            }
+        }
+    }
+
+    /*
+     * return the value that is the kth smallest value of the array.
+     */
+    public static int quickselect(int[] data, int k) {
+        int end = data.length - 1;
+        int start = 0;
+        int x = partition(data, start, end);
+        while (x != k) {
+            x = partition(data, start, end);
+            if (x < k) {
+                start = x;
+            } else if (x > k) {
+                end = x - 1;
+            }
+        }
+        return data[x];
     }
 
     /*
@@ -23,11 +56,14 @@ public class Quick {
      * @return the index of the final position of the pivot element.
      */
     public static int partition(int[] data, int start, int end) {
-        int pIndex = new Random().nextInt(data.length);
+        int pIndex = 0;
+        if (!(end - start == 0)) {
+            pIndex = new Random().nextInt(end - start) + start + 1;
+        }
         int pivot = data[pIndex];
         data[pIndex] = data[start];
         data[start] = pivot;
-        for (int i = start + 1; i <= end; i++) {
+        for (int i = start; i <= end; i++) {
             int x = data[i];
             if (x > pivot) {
                 while (data[end] > pivot) {
