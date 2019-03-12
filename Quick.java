be987effdc2b5c26d65f5807fb220cfbb0e1cecc
AmitPr <17amitprasad@gmail.com>
2019-03-12 19:23:51 -0400

    finished quicksort()

--- Quick.java
+++ Quick.java
@@ -3,16 +3,20 @@ import java.util.Random;
 
 public class Quick {
     public static void main(String[] args) {
-        int size = 1000;
-        int[] test = new int[size];
-        for (int i = 0; i < size; i++) {
-            test[i] = new Random().nextInt(1000000);
-        }
-        int[] sorted = Arrays.copyOf(test, size);
-        Arrays.sort(sorted);
-        for (int i = 0; i < size; i++) {
-            int x = new Random().nextInt(size);
-            if (quickselect(test, x) != sorted[x]) {
+        int size = 10000000;
+        for (int i = 0; i < 1; i++) {
+            int[] test = new int[size];
+            for (int j = 0; j < size; j++) {
+                test[j]=new Random().nextInt(size)- (size / 2);
+            }
+            int[] sorted = Arrays.copyOf(test, size);
+            Arrays.sort(sorted);
+            quicksort(test);
+            boolean fail = false;
+            for(int j =0; j< test.length;j++){
+                if(test[j]!=sorted[j]){fail=true;}
+            }
+            if (fail) {
                 System.out.println("FAIL");
             } else {
                 System.out.println("SUCCESS");
@@ -39,6 +43,20 @@ public class Quick {
     }
 
     /*
+     * Modify the array to be in increasing order.
+     */
+    public static void quicksort(int[] data) {
+        quickHelper(data, 0, data.length-1);
+    }
+    public static void quickHelper(int[]data,int lo, int hi){
+        if(hi-lo > 0){
+            int index = partition(data, lo, hi);
+            quickHelper(data, lo, index-1);
+            quickHelper(data, index+1, hi);
+        }
+    }
+
+    /*
      * Choose a random pivot element between the start and end index inclusive, Then
      * modify the array such that:
      * 
@@ -56,14 +74,17 @@ public class Quick {
      * @return the index of the final position of the pivot element.
      */
     public static int partition(int[] data, int start, int end) {
-        int pIndex = 0;
+        int pIndex = start;
+        int middle = (int) (new Random().nextBoolean() ? Math.ceil((start + end) / 2f)
+                : Math.floor((start + end) / 2f));
+        int median = median(data[start], data[middle], data[end], start, middle, end);
         if (!(end - start == 0)) {
-            pIndex = new Random().nextInt(end - start) + start + 1;
+            pIndex = median;
         }
         int pivot = data[pIndex];
         data[pIndex] = data[start];
         data[start] = pivot;
-        for (int i = start; i <= end; i++) {
+        for (int i = start + 1; i <= end; i++) {
             int x = data[i];
             if (x > pivot) {
                 while (data[end] > pivot) {
@@ -78,6 +99,23 @@ public class Quick {
                 data[i] = data[end];
                 data[end] = x;
             }
+
+            if (x == pivot) {
+                if (new Random().nextBoolean()) {
+                    while (data[end] > pivot) {
+                        if (i != end) {
+                            end--;
+                        } else {
+                            data[start] = data[i - 1];
+                            data[i - 1] = pivot;
+                            return i - 1;
+                        }
+                    }
+                    data[i] = data[end];
+                    data[end] = x;
+                }
+            }
+
             if (i == end) {
                 if (x > pivot) {
                     data[start] = data[i - 1];
@@ -90,5 +128,29 @@ public class Quick {
             }
         }
         return end;
+    }
+
+    public static int median(int a, int b, int c, int aInd, int bInd, int cInd) {
+        if (a > b) {
+            if (a > c) {
+                if (b > c) {
+                    return cInd;
+                } else {
+                    return bInd;
+                }
+            } else {
+                return aInd;
+            }
+        } else {
+            if (a > c) {
+                return aInd;
+            } else {
+                if (b > c) {
+                    return cInd;
+                } else {
+                    return bInd;
+                }
+            }
+        }
     }
 }
